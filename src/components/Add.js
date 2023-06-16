a67b3ff84e56f0580fdda7d2ee2c9fcfca626959
Liirus91 <work.liirus@gmail.com>
2023-06-16 14:15:45 +0300

    feat: add search field on the Add page

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Add = () => {
+  const [query, setQuery] = useState('');
+  const onChange = (e) => {
+    e.preventDefault();
+
+    setQuery(e.target.value);
+  };
+
   return (
-    <div>
-      <h1>Add page</h1>
+    <div className="add-page">
+      <div className="container">
+        <div className="add-content">
+          <div className="input-wrapper">
+            <input
+              type="text"
+              placeholder="Search for a movie"
+              value={query}
+              onChange={onChange}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
