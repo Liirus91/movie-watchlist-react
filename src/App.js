2e88997b3ad20f18f31973a9fd5d46146482de76
Liirus91 <work.liirus@gmail.com>
2023-06-16 14:06:43 +0300

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import './lib/font-awesome/css/all.min.css';
+import { Header } from './components/Header';
+import { Watchlist } from './components/Watchlist';
+import { Watched } from './components/Watched';
+import { Add } from './components/Add';
 
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Watchlist />} />
+
+        <Route exact path="/watched" element={<Watched />} />
+
+        <Route exact path="/add" element={<Add />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
