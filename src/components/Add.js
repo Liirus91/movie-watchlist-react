080bd5645155d2135a2cf1946c48d56c95bfc6e2
Liirus91 <work.liirus@gmail.com>
2023-06-17 09:01:57 +0300

    feat: list of movies when searching by title

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
+import { REACT_APP_TMDB_KEY } from '../constants';
 
 export const Add = () => {
   const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+
   const onChange = (e) => {
     e.preventDefault();
 
     setQuery(e.target.value);
+
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${e.target.value}&api_key=${REACT_APP_TMDB_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.errors) {
+          setResults(data.results);
+        } else {
+          setResults([]);
+        }
+      });
   };
 
   return (
@@ -20,6 +35,14 @@ export const Add = () => {
               onChange={onChange}
             />
           </div>
+
+          {results.length > 0 && (
+            <ul className="results">
+              {results.map((movie) => (
+                <li>{movie.title}</li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </div>
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const REACT_APP_TMDB_KEY = 'b6753f0bae58442f6cbc1ec4be63b390';
