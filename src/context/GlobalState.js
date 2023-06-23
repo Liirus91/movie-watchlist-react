2ed778e0a18b839ddad592e6de18bcab215c5c9f
Liirus91 <work.liirus@gmail.com>
2023-06-23 09:00:33 +0300

    feat: save watched and watchlist in local srorage

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,12 +1,24 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
 import { AppReduser } from './AppReduser';
 
-const initialState = { watchlist: [], watched: [] };
+const initialState = {
+  watchlist: localStorage.getItem('watchlist')
+    ? JSON.parse(localStorage.getItem('watchlist'))
+    : [],
+  watched: localStorage.getItem('watched')
+    ? JSON.parse(localStorage.getItem('watched'))
+    : [],
+};
 
 export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = (props) => {
   const [state, dispatch] = useReducer(AppReduser, initialState);
+
+  useEffect(() => {
+    localStorage.setItem('watchlist', JSON.stringify(state.watchlist));
+    localStorage.setItem('watched', JSON.stringify(state.watched));
+  }, [state]);
 
   const addMovieToWatchlist = (movie) => {
     dispatch({ type: 'ADD_MOVIE_TO_WATCHLIST', payload: movie });
