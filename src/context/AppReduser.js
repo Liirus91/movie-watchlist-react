294e71e2ba7bdcc0d34809905282a700e271b122
Liirus91 <work.liirus@gmail.com>
2023-06-25 18:50:14 +0300

    feat: add removing movie from watchlist

--- src/context/AppReduser.js
+++ src/context/AppReduser.js
@@ -2,6 +2,13 @@ export const AppReduser = (state, action) => {
   switch (action.type) {
     case 'ADD_MOVIE_TO_WATCHLIST':
       return { ...state, watchlist: [action.payload, ...state.watchlist] };
+    case 'REMOVE_MOVIE_FROM_WATCHLIST':
+      return {
+        ...state,
+        watchlist: state.watchlist.filter(
+          (movie) => movie.id !== action.payload
+        ),
+      };
     default:
       return state;
   }
