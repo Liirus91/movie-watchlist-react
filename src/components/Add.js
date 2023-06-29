689342fc6f7c4bd015bf54c070591c20fb12ac78
Liirus91 <work.liirus@gmail.com>
2023-06-29 18:19:42 +0300

    feat: add debounce in search

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,27 +1,34 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { REACT_APP_TMDB_KEY } from '../constants';
 import { ResultCard } from './ResultCard';
+import debounce from 'lodash.debounce';
 
 export const Add = () => {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
 
+  const updateSearchValue = useCallback(
+    debounce((str) => {
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?query=${str}&api_key=${REACT_APP_TMDB_KEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          if (!data.errors) {
+            setResults(data.results);
+          } else {
+            setResults([]);
+          }
+        });
+    }, 500),
+    []
+  );
+
   const onChange = (e) => {
     e.preventDefault();
 
     setQuery(e.target.value);
-
-    fetch(
-      `https://api.themoviedb.org/3/search/movie?query=${e.target.value}&api_key=${REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data.errors) {
-          setResults(data.results);
-        } else {
-          setResults([]);
-        }
-      });
+    updateSearchValue(e.target.value);
   };
 
   return (
